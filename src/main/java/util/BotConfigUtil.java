b56443bb3b356d76538132592daa7460ac16952f
Nikita Kondrakhin <kondrakhin94@gmail.com>
2024-02-15 17:17:53 +0600

    Add logging, minor changes in BotConfigUtil.java

--- src/main/java/util/BotConfigUtil.java
+++ src/main/java/util/BotConfigUtil.java
@@ -2,32 +2,43 @@ package util;
 
 import exceptions.PropertiesParsingException;
 import lombok.experimental.UtilityClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.Properties;
 
 @UtilityClass
 public class BotConfigUtil {
+    private final String PROPERTIES_FILE_NAME = "bot.properties";
+    private final String BOT_USERNAME_KEY = "botUsername";
+    private final String BOT_TOKEN_KEY = "botToken";
+    private final Logger logger = LoggerFactory.getLogger(BotConfigUtil.class);
     private final Properties botProperties = loadProperties();
+
+    public static String getBotUsername() {
+        return botProperties.getProperty(BOT_USERNAME_KEY);
+    }
+
+    public static String getBotToken() {
+        return botProperties.getProperty(BOT_TOKEN_KEY);
+    }
+
     private static Properties loadProperties() {
-        try (InputStream input = BotConfigUtil.class.getClassLoader().getResourceAsStream("bot.properties")) {
+        try (InputStream input = BotConfigUtil.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE_NAME)) {
             Properties properties = new Properties();
             if (input != null) {
                 properties.load(input);
             } else {
-                throw new PropertiesParsingException("Properties file not found");
+                String errorMessage = String.format("Properties file %s not found", PROPERTIES_FILE_NAME);
+                logger.error(errorMessage);
+                throw new PropertiesParsingException(errorMessage);
             }
             return properties;
         } catch (IOException e) {
-            throw new PropertiesParsingException("Error loading bot properties", e);
+            String errorMessage = String.format("Error loading properties from %s file", PROPERTIES_FILE_NAME);
+            logger.error(errorMessage);
+            throw new PropertiesParsingException(errorMessage, e);
         }
-    }
-
-    public static String getBotUsername() {
-        return botProperties.getProperty("botUsername");
-    }
-
-    public static String getBotToken() {
-        return botProperties.getProperty("botToken");
     }
 }
